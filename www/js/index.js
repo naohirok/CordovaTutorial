9ce68509a331fe88003b457e4d2c5817b99deab8
Kubota Naohiro <naohirok.1726@gmail.com>
2017-10-18 16:38:54 +0900

    add camera

--- www/js/index.js
+++ www/js/index.js
@@ -90,3 +90,43 @@ document.addEventListener("backbutton", pressBackButton, false);
 function onBatteryStatus(info) {
     alert("BATTERY STATUS:  Level: " + info.level + " isPlugged: " + info.isPlugged);
 }
+
+// camera
+
+function cameraTakePicture() {
+    navigator.camera.getPicture(onSuccess, onFail, {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL
+    });
+
+    function onSuccess(imageData) {
+        var image = document.getElementById('myImage');
+        image.src = "data:image/jpeg;base64," + imageData;
+    }
+
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+}
+
+document.getElementById("cameraTakePicture").addEventListener("click", cameraTakePicture);
+
+function cameraGetPicture() {
+
+    navigator.camera.getPicture(onSuccess, onFail, {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+    });
+
+    function onSuccess(imageURL) {
+        var image = document.getElementById('myImage');
+        image.src = imageURL;
+    }
+
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+}
+
+document.getElementById("cameraGetPicture").addEventListener("click", cameraGetPicture);
